91306054e545b80fdb000ec745ae8f0480fb8291
maidenatorrent <123751590+maidenator@users.noreply.github.com>
2026-05-10 21:51:11 +0800

    Delete Assignment41226 directory

--- Assignment41226/app/src/main/java/com/example/assignment_4_12_26/data/models/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.assignment_4_12_26.data.models
-
-data class User(
-    val id: String,
-    val fullName: String,
-    val email: String,
-    val questionsAsked: Int = 0,
-    val upvotesReceived: Int = 0
-)
